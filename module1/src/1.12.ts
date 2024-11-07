9ad43c4d137ed2446a558535f50e79b78feec563
husneara <husnearaa99@gmail.com>
2024-11-07 14:12:55 +0600

    trying git upload

---
+++ module1/src/1.12.ts
@@ -0,0 +1,29 @@
+{
+
+
+// nullable types
+
+const searchName = (value: string) =>{
+   if(value){
+    console.log("Searching");
+   } else{
+    console.log("There is nothing searching");
+   }
+}
+
+
+searchName('hosneara');
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
