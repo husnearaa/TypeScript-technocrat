d42d1b63fbb65425ac35466ab9d64f61d4df4f11
husneara <husnearaa99@gmail.com>
2024-11-07 14:41:20 +0600

    nullable types

---
+++ README.md
@@ -0,0 +1,3 @@
+This is the npm package. in one commit you can push your code to the github
+
+https://www.npmjs.com/package/ts-node-dev
--- module1/src/1.12.ts
+++ module1/src/1.12.ts
@@ -1,9 +1,9 @@
 {
 
 
-// nullable types
+// nullable types / unknown type
 
-const searchName = (value: string) =>{
+const searchName = (value: string | null) =>{
    if(value){
     console.log("Searching");
    } else{
@@ -11,14 +11,40 @@ const searchName = (value: string) =>{
    }
 }
 
-
-searchName('hosneara');
-
+searchName(null);
 
 
 
+const getSpeedMeterPerSecond = (value:unknown)=>{
+    if(typeof value === 'number'){
+        const convertedSpeed = (value * 1000) / 3600;
+        console.log(`the speed is ${convertedSpeed} ms^-1`)
+    }
+
+    if(typeof value === "string"){
+        const [result, unit] = value.split(" ");
+        const convertedSpeed = (parseFloat(result) * 1000) / 3600;
+        console.log(`the speed is ${convertedSpeed} ms^-1`)
+    }
+    else{
+        console.log("wring input");
+    }
+};
+
+getSpeedMeterPerSecond(`1000 km^-1`);
 
 
+
+
+
+
+// never
+
+const  throwError=(msg: string):never =>{
+throw new Error(msg);
+};
+
+throwError('muskil se error hogaya');
 
 
 
